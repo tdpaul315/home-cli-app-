a356fb27793c7a695d6a16d042b681d20455a01f
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-07-31 16:29:07 -0400

    sets class variable stats equal to an empty array

--- lib/stats.rb
+++ lib/stats.rb
@@ -2,12 +2,15 @@ class Stats
   attr_accessor :county_name
   attr_reader :reported_cases, :hospitalizations, :deaths
 
+  @@stats = []
+
 
   def initialize(county_name)
     @county_name = county_name
     @reported_cases = reported_cases
     @hospitalizations = hospitalizations
     @deaths = deaths
+    @@stats << self 
   end
 
   
