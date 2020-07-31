d96b42fcbbebeb7b8696b9d78580b033cba83377
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-07-31 15:51:06 -0400

    uses attr_accessor and attr_reader to create methods for attributes of the stats class

--- lib/stats.rb
+++ lib/stats.rb
@@ -1,12 +1,13 @@
 class Stats
-  attr_accessor :county_name, :reported_cases, :hospitalizations, :deaths
+  attr_accessor :county_name
+  attr_reader :reported_cases, :hospitalizations, :deaths
 
 
-  def initialize(county_name, reported_cases, hospitalizations, deaths)
-    @county_name = county_name 
+  def initialize(county_name)
+    @county_name = county_name
     @reported_cases = reported_cases
     @hospitalizations = hospitalizations
-    @deaths = deaths 
+    @deaths = deaths
   end
 
   
