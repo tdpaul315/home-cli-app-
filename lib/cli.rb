b8f1a24e4638f163ee91fc918c0495ef19a964b6
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-08-02 21:02:52 -0400

    condition added to present_user_option method to check the user's input

--- lib/cli.rb
+++ lib/cli.rb
@@ -21,13 +21,13 @@ class CLI
     def present_user_option # WORKING 
        puts "Would you like to check another county?"
        puts "Please enter Y for Yes or N for No"
-        #user_input = gets.strip
-        #if user_input == "Y"
-           # get_county_name
-           #present_user_option
-       # else user_input == "N"
-          # exit_and_goodbye
-        #end 
+        user_input = gets.strip
+        if user_input == "Y"
+            get_county_name
+           present_user_option
+        else user_input == "N"
+           exit_and_goodbye
+        end 
     end 
 
     #def check_input(county_name) #WORKING - error handling
