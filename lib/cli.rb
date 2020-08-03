baf9a6ec448a835a8132eca9494af2b64642a2e1
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-08-02 21:01:13 -0400

    defines the prompt_user_option method with greeting and prompt

--- lib/cli.rb
+++ lib/cli.rb
@@ -18,17 +18,17 @@ class CLI
        return county_name
     end
 
-    #def present_user_option # WORKING 
-     #   puts "Would you like to check another county?"
-      #  puts "Please enter Y for Yes or N for No"
-       # user_input = gets.strip
+    def present_user_option # WORKING 
+       puts "Would you like to check another county?"
+       puts "Please enter Y for Yes or N for No"
+        #user_input = gets.strip
         #if user_input == "Y"
-         #   get_county_name
-          #  present_user_option
-        #else user_input == "N"
-         #   exit_and_goodbye
+           # get_county_name
+           #present_user_option
+       # else user_input == "N"
+          # exit_and_goodbye
         #end 
-    #end 
+    end 
 
     #def check_input(county_name) #WORKING - error handling
      #  if API.counties.include?(county_name.downcase)
